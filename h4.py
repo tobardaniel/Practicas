46b4bc7388ce30d03cbe8754babbb632e0387006
tobardaniel <53271453+tobardaniel@users.noreply.github.com>
2019-11-02 23:19:07 -0600

    Funciona palíndromo; simple sum,  minimax y base se tardan demasiado.

--- h4.py
+++ h4.py
@@ -1,6 +1,7 @@
 import math
 import sys
 import time
+primos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443]
 
 
 def cifras(numero):
@@ -15,7 +16,7 @@ def cifras(numero):
 
 def simple_sum(numero, mod):
     suma = 0
-    for i in range(1, numero):
+    for i in range(1, numero+1):
         potencia = pow(i, i, mod)
         suma = suma + potencia
     return suma % mod
@@ -23,7 +24,7 @@ def simple_sum(numero, mod):
 
 def simple_sum1(numero, mod):
     suma = 0
-    for i in range(1, numero):
+    for i in range(1, numero + 1):
         arr_cifras = cifras(i)[::-1]
         potencia = 1
         for k in arr_cifras:
@@ -32,13 +33,23 @@ def simple_sum1(numero, mod):
     return suma % mod
 
 
+
+
+
+
+
+
+
+
+
+
 start = time.time()
-resultado2 = simple_sum(654321, 5465)
+resultado2 = simple_sum(1000000, 5465)
 end = time.time()
 tiempo2 = end-start
 print('Lento: ', resultado2, ' con tiempo de: ', tiempo2)
 start = time.time()
-resultado2 = simple_sum1(654321, 5465)
+resultado2 = simple_sum1(1000000, 5465)
 end = time.time()
 tiempo2 = end-start
 print('Rápido: ', resultado2, ' con tiempo de: ', tiempo2)
---
+++ minimax.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def leer_matriz(n):
+    matriz = []
+    for i in range(n):
+        fila = [int(s) for s in input().split(' ')]
+        matriz.append(fila)
+    matriz = np.array(matriz)
+    return matriz
+
+
+def ordenar(matriz, arg='F'):
+    if arg == 'F':
+        matriz = np.array([sorted(s) for s in matriz])
+        matriz = matriz.transpose()
+        return matriz
+    elif arg == 'C':
+        matriz = matriz.transpose()
+        matriz = np.array([sorted(s)[::-1] for s in matriz])
+        matriz = matriz.transpose()
+        return matriz
+
+
+def minimax(matriz):
+    cambios = len(matriz)
+    n = 0
+    matriz_filas = ordenar(matriz, 'F')
+    matriz_columnas = ordenar(matriz, 'C')
+    if min(matriz_columnas[0]) == max(matriz_filas[0]):
+        return('0')
+    elif min(matriz_columnas[0]) < max(matriz_filas[0]):
+        fallo = True
+        while fallo and n <= len(matriz):
+            for k in range(n):
+                if min(matriz_columnas[k]) >= max(matriz_filas[n-k]):
+                    cambios = n
+                    fallo = False
+            n += 1
+        return cambios
+    elif min(matriz_columnas[0]) > max(matriz_filas[0]):
+        fallo = True
+        while fallo and n <= len(matriz):
+            for k in range(n):
+                if min(matriz_columnas[k]) <= max(matriz_filas[n - k]):
+                    cambios = n
+                    fallo = False
+            n += 1
+        return cambios
+
+
+
+matriz = leer_matriz(int(input()))
+print(minimax(matriz))
